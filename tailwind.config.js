376eedf6171b8c0cb81dfe5081e7fe35289d0f0a
Flibouche <kevin.pfiffer2@gmail.com>
2024-06-20 11:58:38 +0200

    Add GameController, add custom font for visual impairment and dyslexia.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'luciole' : ['Luciole', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
