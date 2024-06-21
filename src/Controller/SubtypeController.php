52a09033a506a7fd0890527b3ff70f2cb5692fd3
Flibouche <kevin.pfiffer2@gmail.com>
2024-06-21 11:56:34 +0200

    Add my form to my Subtype view.

--- src/Controller/SubtypeController.php
+++ src/Controller/SubtypeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Subtype;
+use App\Form\SubtypeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,6 +16,19 @@ class SubtypeController extends AbstractController
     {
         return $this->render('subtype/index.html.twig', [
             'controller_name' => 'SubtypeController',
+        ]);
+    }
+
+    #[Route('/subtype', name: 'app_subtype')]
+    public function addSubtypeToGame(Subtype $subtype = null, Request $request)
+    {
+
+        $form = $this->createForm(SubtypeType::class, $subtype);
+
+        $form->handleRequest($request);
+
+        return $this->render('subtype/index.html.twig', [
+            'formAddSubtype' => $form,
         ]);
     }
 }
