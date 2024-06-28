1b9ea702fdd531c821ded126d8d04ef5b585f1e0
Flibouche <kevin.pfiffer2@gmail.com>
2024-06-29 00:19:39 +0200

    Add some css : move my main div on the base html and create table for my gamelist.

--- src/Service/IgdbApiService.php
+++ src/Service/IgdbApiService.php
@@ -30,7 +30,7 @@ class IgdbApiService
         $this->logger->info('Access token obtained', ['access_token' => $accessToken]);
 
         // Construction de la requête pour l'API IGDB
-        $queryString = "fields id,name,cover.url; limit 50;";
+        $queryString = "fields id,name; limit 50;";
         if ($name) {
             $queryString = "search \"$name\"; " . $queryString;
         }
@@ -72,7 +72,7 @@ class IgdbApiService
         $this->logger->info('Access token obtained', ['access_token' => $accessToken]);
     
         // Construction de la requête pour l'API IGDB
-        $queryString = "fields id,name; where id = $idGameApi;";
+        $queryString = "fields id,name,cover.image_id; where id = $idGameApi;";
     
         // Envoi de la requête à l'API IGDB
         $response = $this->httpClient->request('POST', 'https://api.igdb.com/v4/games', [
