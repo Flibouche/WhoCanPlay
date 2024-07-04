ee66ed0471806d41f634701db357e49b5b8182c5
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-04 10:45:35 +0200

    Add cache tests to HomeController and framework yaml.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
+    #[Cache(public: true, maxage: 3600, mustRevalidate: true)]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
