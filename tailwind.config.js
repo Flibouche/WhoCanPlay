526d89f526e66651774fc4327c44b30570a717c1
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-05 16:53:44 +0200

    Remove media queries xs, try css on the show game page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,6 @@ module.exports = {
         'luciole': ['Luciole', 'sans-serif'],
       },
     },
-    screens: {
-      'xs': '376px',
-      // => @media (min-width: 376px) { ... }
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }
   },
   plugins: [],
 }
