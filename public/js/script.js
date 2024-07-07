8ea875dc4caa364369be0f9dc31392c9666b973f
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-07 22:05:13 +0200

    Add dark-mode switch with localStorage gestion.

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,37 @@
+// Dark Mode
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#toggle");
 
-const modal = document.querySelector('#modal') ?? null;
-const openModal = document.querySelector('#open-button') ?? null;
-const closeModal = document.querySelector('#close-button') ?? null;
-const body = document.querySelector('body');
+  // Je check le thème initial du localStorage
+  const darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "enabled") {
+    document.documentElement.classList.add("dark");
+  }
 
-if(openModal && closeModal) {
-    openModal.addEventListener('click', () => {
-        modal.showModal();
-        body.classList.add('blur-sm')
-    })
-    
-    closeModal.addEventListener('click', () => {
-        modal.close();
-        body.classList.remove('blur-sm')
-    })
+  // J'ajoute le darkMode et je sauvegarde les préférences dans le localStorage
+  button.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      localStorage.setItem("dark-mode", "disabled");
+    }
+  });
+});
+
+const modal = document.querySelector("#modal") ?? null;
+const openModal = document.querySelector("#open-button") ?? null;
+const closeModal = document.querySelector("#close-button") ?? null;
+const body = document.querySelector("body");
+
+if (openModal && closeModal) {
+  openModal.addEventListener("click", () => {
+    modal.showModal();
+    body.classList.add("blur-sm");
+  });
+
+  closeModal.addEventListener("click", () => {
+    modal.close();
+    body.classList.remove("blur-sm");
+  });
 }
