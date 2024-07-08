259838cd2a4b187cf91206e9d7ee8e14ec4cd441
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-08 14:58:37 +0200

    Add mobile menu with animation.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,22 @@
+// Menu Burger
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const closeMenu = document.getElementById('closeMenu');
+    const mobileMenuContent = document.getElementById('mobileMenuContent');
+
+    menuToggle.addEventListener('click', function() {
+        mobileMenuContent.classList.remove('translate-x-full');
+    });
+
+    closeMenu.addEventListener('click', function() {
+        mobileMenuContent.classList.add('translate-x-full')
+    })
+});
+
 // Dark Mode
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("#toggle");
+  const button = document.querySelector("#toggle"); // Élément normal
+  const buttonMobileMenu = document.querySelector("#toggleMobileMenu"); // Élément mobile
 
   // Je check le thème initial du localStorage
   const darkMode = localStorage.getItem("dark-mode");
@@ -10,6 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // J'ajoute le darkMode et je sauvegarde les préférences dans le localStorage
   button.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      localStorage.setItem("dark-mode", "disabled");
+    }
+  });
+
+  // Pareil qu'en haut mais en vue mobile
+  buttonMobileMenu.addEventListener("click", () => {
     document.documentElement.classList.toggle("dark");
     if (document.documentElement.classList.contains("dark")) {
       localStorage.setItem("dark-mode", "enabled");
