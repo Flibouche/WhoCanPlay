d41137897ca5301cd371201c623086fcc99d54d8
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-09 23:33:14 +0200

    Add 3 lasts games added to the home page, add CSS and add 2 breadcrumbs.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -20,6 +20,7 @@ class DashboardController extends AbstractDashboardController
 {
     public function __construct(private AdminUrlGenerator $adminUrlGenerator)
     {
+        
     }
 
     #[Route('/admin', name: 'admin')]
@@ -31,6 +32,17 @@ class DashboardController extends AbstractDashboardController
         $url = $this->adminUrlGenerator->setController(DisabilityCrudController::class)->generateUrl();
 
         return $this->redirect($url);
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -43,40 +55,39 @@ class DashboardController extends AbstractDashboardController
     {
         // La méthode yield permet de retourner un élément comme un tableau mais sans l'usage du mot clé "return"
         yield MenuItem::section('Disabilities');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create disability', 'fas fa-plus', Disability::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show disability', 'fas fa-eye', Disability::class),
-            ]);
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create disability', 'fas fa-plus', Disability::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show disability', 'fas fa-eye', Disability::class),
+        ]);
 
         yield MenuItem::section('Features');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create feature', 'fas fa-plus', Feature::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show feature', 'fas fa-eye', Feature::class),
-            ]);
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create feature', 'fas fa-plus', Feature::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show feature', 'fas fa-eye', Feature::class),
+        ]);
 
         yield MenuItem::section('Games');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create game', 'fas fa-plus', Game::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show game', 'fas fa-eye', Game::class),
-            ]);
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create game', 'fas fa-plus', Game::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show game', 'fas fa-eye', Game::class),
+        ]);
 
         yield MenuItem::section('Images');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create image', 'fas fa-plus', Image::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show image', 'fas fa-eye', Image::class),
-            ]);
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create image', 'fas fa-plus', Image::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show image', 'fas fa-eye', Image::class),
+        ]);
 
         yield MenuItem::section('Posts');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create post', 'fas fa-plus', Post::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show post', 'fas fa-eye', Post::class),
-            ]);
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create post', 'fas fa-plus', Post::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show post', 'fas fa-eye', Post::class),
+        ]);
 
         yield MenuItem::section('Topics');
-            yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create topic', 'fas fa-plus', Topic::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Show topic', 'fas fa-eye', Topic::class),
-            ]);
-            
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Create topic', 'fas fa-plus', Topic::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show topic', 'fas fa-eye', Topic::class),
+        ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
+use App\Service\IgdbApiService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpKernel\Attribute\Cache;
@@ -9,11 +11,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
+    private $igdbApiService;
+
+    public function __construct(IgdbApiService $igdbApiService)
+    {
+        $this->igdbApiService = $igdbApiService;
+    }
+
     #[Route('/home', name: 'app_home')]
     #[Cache(public: true, maxage: 3600, mustRevalidate: true)]
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
+        // Je cherche directement les jeux ayant le status 1, soit les jeux actifs, je les range par ID la plus récente et je limite à 3
+        $activeGames = $gameRepository->findBy(['status' => 1], ['id' => 'DESC'], 3);
+
+        // Je crée un tableau vide pour y stocker les IdGameApi
+        $gameApiIds = [];
+        foreach ($activeGames as $game) {
+            $gameApiIds[] = $game->getIdGameApi();
+        }
+
+        // Avec mon nouveau tableau, je récupère les informations via le service de l'API en y passant mon tableau en argument
+        $gamesApiData = $this->igdbApiService->getGameByIds($gameApiIds);
+
+        // Je crée un nouveau tableau pour pouvoir indexer les jeux par leur ID correspondante grâce aux ID que j'ai récupéré de mon service 
+        $gamesApiDataById = [];
+        foreach ($gamesApiData as $game) {
+            $gamesApiDataById[$game['id']] = $game;
+        }
+
+        // Je crée un tableau vide pour y stocker les informations combinées des jeux de ma BDD et des jeux de l'API
+        $gamesApiInfo = [];
+
+        // Je vérifie si la liste des jeux actifs n'est pas vide
+        if ($activeGames) {
+            foreach ($activeGames as $gameDb) {
+                // Pour chaque jeu actif, je récupère l'idGameApi
+                $idGameApi = $gameDb->getIdGameApi();
+
+                // Je récupère les informations du jeu à partir du tableau indexé que j'ai crée plus haut
+                $gameApi = $gamesApiDataById[$idGameApi] ?? null;
+
+                // J'ajoute un tableau associatif à mon tableau $gamesApiInfo qui contient maintenant les informations combinées des jeux de ma BDD et des jeux de l'API
+                $gamesApiInfo[] = [
+                    'db' => $gameDb,
+                    'api' => $gameApi,
+                ];
+            }
+        } else {
+            // Si aucun jeu actif n'est trouvé, j'ajoute un message flash "No game found !"
+            $this->addFlash('warning', "No game found !");
+        }
+
         return $this->render('home/index.html.twig', [
+            'games' => $gamesApiInfo,
             'controller_name' => 'HomeController',
         ]);
     }
