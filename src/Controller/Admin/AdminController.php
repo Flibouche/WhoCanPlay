c5fbc38057d6d4b365a188ab6f2d3c238d97b23e
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-26 11:31:38 +0200

    Add admin-secret to the AdminController.

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,15 +5,22 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/admin', name: 'app_admin_')]
+#[Route('/admin-{secret}', name: 'app_admin_')]
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(string $secret): Response
     {
+        $expectedSecret = $this->getParameter('admin_secret');
+        if ($secret !== $expectedSecret) {
+            throw $this->createAccessDeniedException('Page not found');
+        }
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'admin_secret' => $expectedSecret,
         ]);
     }
 }
