760bdd1861a29b86c64b010d4b084af779675e5e
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-26 11:57:37 +0200

    Add template to my admin views.

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -20,7 +20,6 @@ class AdminController extends AbstractController
         }
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-            'admin_secret' => $expectedSecret,
         ]);
     }
 }
--- src/Controller/Admin/FeatureAdminController.php
+++ src/Controller/Admin/FeatureAdminController.php
@@ -1,8 +1,19 @@
 <?php
 
-// namespace App\Admin\Controller;
+namespace App\Controller\Admin;
 
-// class FeatureAdminController extends AbstractController
-// {
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-// }
+#[Route('/admin/feature', name: 'app_admin_feature_')]
+class FeatureAdminController extends AbstractController
+{
+    #[Route('/', name: 'show')]
+    public function index(): Response
+    {
+        return $this->render('admin/features/show.html.twig', [
+            'controller_name' => 'FeatureAdminController',
+        ]);
+    }
+}
