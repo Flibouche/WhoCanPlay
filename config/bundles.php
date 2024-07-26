50d1fae25543111b1b26bab8487c3ecbfd2bc8a9
Flibouche <kevin.pfiffer2@gmail.com>
2024-07-26 11:03:21 +0200

    Remove easyAdmin and create my own admin dashboard.

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin', name: 'app_admin_')]
 class AdminController extends AbstractController
 {
-    // #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
