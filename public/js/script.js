4f68488c36444308474866c44789692fe589a174
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-24 14:55:07 +0200

    Change id name of divs of the searchbar.

--- public/js/script.js
+++ public/js/script.js
@@ -46,44 +46,44 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Search bar
-let routeTemplate = "{{ path('show_game', { 'id': 'ID_PLACEHOLDER', 'slug': 'SLUG_PLACEHOLDER' }) }}";
-$(document).ready(function () {
-    var searchTimeout;
-    $('#search').on('input', function () {
-        var query = $(this).val();
-        clearTimeout(searchTimeout);
-        if (query.length > 2) {
-            searchTimeout = setTimeout(function () {
-                $.ajax({
-                    url: '/searchGame',
-                    type: 'GET',
-                    data: { search: query },
-                    success: function (data) {
-                        var resultDiv = $('#result');
-                        resultDiv.empty();
-                        if (data.length > 0) {
-                            var list = $('<ul class="absolute w-[100%] z-50 p-0 m-0 bg-gray-700"></ul>');
-                            data.forEach(function (item) {
-                                let itemUrl = routeTemplate.replace('ID_PLACEHOLDER', item.id).replace('SLUG_PLACEHOLDER', item.slug);
-                                list.append(
-                                    '<li class="result-item flex items-center px-2 justify-between cursor-pointer hover:bg-indigo-200 my-2" data-id="' + item.id + '">' +
-                                    '<a href="' + itemUrl + '" class="flex items-center justify-between w-full">' +
-                                    '<span class="name-date">' + item.name + '</span>' +
-                                    `<img loading="lazy" src='https://images.igdb.com/igdb/image/upload/t_thumb/${item.imageUrl}.jpg' alt='${item.name} cover' class="w-20 h-10 object-cover">` +
-                                    '</a>' +
-                                    '</li>'
+// let routeTemplate = "{{ path('show_game', { 'id': 'ID_PLACEHOLDER', 'slug': 'SLUG_PLACEHOLDER' }) }}";
+// $(document).ready(function () {
+//     var searchTimeout;
+//     $('#search').on('input', function () {
+//         var query = $(this).val();
+//         clearTimeout(searchTimeout);
+//         if (query.length > 2) {
+//             searchTimeout = setTimeout(function () {
+//                 $.ajax({
+//                     url: '/searchGame',
+//                     type: 'GET',
+//                     data: { search: query },
+//                     success: function (data) {
+//                         var resultDiv = $('#result');
+//                         resultDiv.empty();
+//                         if (data.length > 0) {
+//                             var list = $('<ul class="absolute w-[100%] z-50 p-0 m-0 bg-gray-700"></ul>');
+//                             data.forEach(function (item) {
+//                                 let itemUrl = routeTemplate.replace('ID_PLACEHOLDER', item.id).replace('SLUG_PLACEHOLDER', item.slug);
+//                                 list.append(
+//                                     '<li class="result-item flex items-center px-2 justify-between cursor-pointer hover:bg-indigo-200 my-2" data-id="' + item.id + '">' +
+//                                     '<a href="' + itemUrl + '" class="flex items-center justify-between w-full">' +
+//                                     '<span class="name-date">' + item.name + '</span>' +
+//                                     `<img loading="lazy" src='https://images.igdb.com/igdb/image/upload/t_thumb/${item.imageUrl}.jpg' alt='${item.name} cover' class="w-20 h-10 object-cover">` +
+//                                     '</a>' +
+//                                     '</li>'
 
-                                );
-                            });
-                            resultDiv.append(list);
-                        } else {
-                            $('#result').html('<div>No result found</div>');
-                        }
-                    }
-                });
-            }, 300);
-        } else {
-            $('#result').empty();
-        }
-    });
-});
+//                                 );
+//                             });
+//                             resultDiv.append(list);
+//                         } else {
+//                             $('#result').html('<div>No result found</div>');
+//                         }
+//                     }
+//                 });
+//             }, 300);
+//         } else {
+//             $('#result').empty();
+//         }
+//     });
+// });
