31e4c11a2f395eec159484a44edb5169c45c6ac9
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-26 11:33:44 +0200

    Change mail adress to whocanplay in the RegistrationController.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,7 +60,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('admin@whocanuse.com', 'WhoCanUse Email Verifier'))
+                    ->from(new Address('admin@whocanplay.com', 'WhoCanUse Email Verifier'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
