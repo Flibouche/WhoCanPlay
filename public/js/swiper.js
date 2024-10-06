c7a0c2ca103fa619f98d2826cbac4b4ef88e0d7d
Flibouche <kevin.pfiffer2@gmail.com>
2024-10-06 11:01:13 +0200

    Add swiper to the showFeature view.

---
+++ public/js/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    zoom: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
